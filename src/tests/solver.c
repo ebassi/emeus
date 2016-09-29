28dd56762ec0ff5e32718c3fc29c28ef72b382ff
Emmanuele Bassi <ebassi@gnome.org>
2016-09-29 20:36:39 +0100

    tests: Add units for solving equalities
    Test that we get an equality between a variable and a constant, and that
    we can solve a constraint with a stay variable.

--- src/tests/solver.c
+++ src/tests/solver.c
@@ -115,6 +115,58 @@ emeus_solver_variable_leq_constant (void)
   simplex_solver_clear (&solver);
 }
 
+static void
+emeus_solver_variable_eq_constant (void)
+{
+  SimplexSolver solver = SIMPLEX_SOLVER_INIT;
+
+  simplex_solver_init (&solver);
+
+  Variable *x = simplex_solver_create_variable (&solver, "x", 10.0);
+  Expression *e = simplex_solver_create_expression (&solver, 100.0);
+
+  simplex_solver_add_constraint (&solver, x, OPERATOR_TYPE_EQ, e, STRENGTH_REQUIRED);
+
+  double x_value = variable_get_value (x);
+
+  emeus_assert_fuzzy_equals (x_value, 100.0, 1e-8);
+
+  expression_unref (e);
+  variable_unref (x);
+
+  simplex_solver_clear (&solver);
+}
+
+static void
+emeus_solver_eq_with_stay (void)
+{
+  SimplexSolver solver = SIMPLEX_SOLVER_INIT;
+
+  simplex_solver_init (&solver);
+
+  Variable *x = simplex_solver_create_variable (&solver, "x", 10.0);
+  Variable *width = simplex_solver_create_variable (&solver, "width", 10.0);
+  Variable *right_min = simplex_solver_create_variable (&solver, "rightMin", 100.0);
+  Expression *right = expression_plus_variable (expression_new_from_variable (x), width);
+
+  simplex_solver_add_stay_variable (&solver, width, STRENGTH_WEAK);
+  simplex_solver_add_stay_variable (&solver, right_min, STRENGTH_WEAK);
+  simplex_solver_add_constraint (&solver, right_min, OPERATOR_TYPE_EQ, right, STRENGTH_REQUIRED);
+
+  double x_value = variable_get_value (x);
+  double width_value = variable_get_value (width);
+
+  emeus_assert_fuzzy_equals (x_value, 90.0, 1e-8);
+  emeus_assert_fuzzy_equals (width_value, 10.0, 1e-8);
+
+  expression_unref (right);
+  variable_unref (right_min);
+  variable_unref (width);
+  variable_unref (x);
+
+  simplex_solver_clear (&solver);
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -124,6 +176,8 @@ main (int argc, char *argv[])
   g_test_add_func ("/emeus/solver/stay", emeus_solver_stay);
   g_test_add_func ("/emeus/solver/variable-geq-constant", emeus_solver_variable_geq_constant);
   g_test_add_func ("/emeus/solver/variable-leq-constant", emeus_solver_variable_leq_constant);
+  g_test_add_func ("/emeus/solver/variable-eq-constant", emeus_solver_variable_eq_constant);
+  g_test_add_func ("/emeus/solver/eq-with-stay", emeus_solver_eq_with_stay);
 
   return g_test_run ();
 }
