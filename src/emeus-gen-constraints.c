32cdf042a2e6170845512b51d6ecd5b3c7da9b2e
Emmanuele Bassi <ebassi@gnome.org>
2016-10-31 17:47:42 +0000

    Update emeus-gen-constraints
    We need to keep up with VflParser's new behaviour.
    
    The tool now takes:
    
     * command line options for default horizontal and vertical spaces
     * command line options for a list of views
     * a list of VFL format strings as input
    
    No custom metrics, at the moment, because the command line handling for
    that is going to be a bit tricky.

--- src/emeus-gen-constraints.c
+++ src/emeus-gen-constraints.c
@@ -50,6 +50,21 @@ strength_to_string (StrengthType strength)
   return "weak";
 }
 
+static int opt_hspacing = -1;
+static int opt_vspacing = -1;
+static char **opt_views;
+static char **opt_vfl;
+
+static GOptionEntry options[] = {
+  { "hspacing", 'H', 0, G_OPTION_ARG_INT, &opt_hspacing, "Default horizontal spacing", "SPACING" },
+  { "vspacing", 'V', 0, G_OPTION_ARG_INT, &opt_vspacing, "Default vertical spacing", "SPACING" },
+  { "view", 0, 0, G_OPTION_ARG_STRING_ARRAY, &opt_views, "Views", "NAME" },
+
+  { G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_STRING_ARRAY, &opt_vfl, "Visual Format Language strings", "FORMAT" },
+
+  { NULL, },
+};
+
 int
 main (int argc, char *argv[])
 {
@@ -61,54 +76,82 @@ main (int argc, char *argv[])
       die ();
     }
 
-  VflParser *parser = vfl_parser_new ();
-  const char *format = argv[1];
+  GOptionContext *context = g_option_context_new (" - Generate constraint descriptions");
+  g_option_context_add_main_entries (context, options, NULL);
+  g_option_context_set_help_enabled (context, TRUE);
+  g_option_context_set_ignore_unknown_options (context, FALSE);
+
   GError *error = NULL;
-
-  vfl_parser_parse_line (parser, format, -1, &error);
-  if (error != NULL)
+  if (!g_option_context_parse (context, &argc, &argv, &error))
     {
-      int offset = vfl_parser_get_error_offset (parser);
-      int range = vfl_parser_get_error_range (parser);
-
-      char *squiggly = NULL;
-
-      if (range > 0)
-        {
-          squiggly = g_new (char, range + 1);
-          for (int i = 0; i < range; i++)
-            squiggly[i] = '~';
-          squiggly[range] = '\0';
-        }
-
-      fprintf (stderr, "%s: error: %s\n", argv[0], error->message);
-      fprintf (stderr, "%s\n", argv[1]);
-      fprintf (stderr, "%*s^%s\n", offset, " ", squiggly != NULL ? squiggly : "");
-
-      g_free (squiggly);
-
+      fprintf (stderr, "ERROR: %s\n", error->message);
+      print_usage (argv[0]);
       die ();
     }
 
-  int n_constraints = 0;
-  VflConstraint *constraints = vfl_parser_get_constraints (parser, &n_constraints);
+  GHashTable *views = g_hash_table_new (g_str_hash, g_str_equal);
+
+  for (int i = 0; opt_views[i] != NULL; i++)
+    g_hash_table_add (views, opt_views[i]);
+
+  VflParser *parser = vfl_parser_new ();
+
+  vfl_parser_set_default_spacing (parser, opt_hspacing, opt_vspacing);
+  vfl_parser_set_metrics (parser, NULL);
+  vfl_parser_set_views (parser, views);
 
   fprintf (stdout, "<constraints>\n");
-  for (int i = 0; i < n_constraints; i++)
+
+  for (int i = 0; opt_vfl[i] != NULL; i++)
     {
-      VflConstraint *c = &constraints[i];
+      GError *error = NULL;
 
-      fprintf (stdout, "  <constraint target-object=\"%s\" target-attr=\"%s\"\n", c->view1, c->attr1);
-      fprintf (stdout, "              relation=\"%s\"\n", relation_to_string (c->relation));
+      vfl_parser_parse_line (parser, opt_vfl[i], -1, &error);
+      if (error != NULL)
+        {
+          int offset = vfl_parser_get_error_offset (parser);
+          int range = vfl_parser_get_error_range (parser);
 
-      if (c->view2 != NULL)
-        fprintf (stdout, "              source-object=\"%s\" source-attr=\"%s\"\n", c->view2, c->attr2);
+          char *squiggly = NULL;
 
-      fprintf (stdout, "              constant=\"%g\" multiplier=\"%g\"\n", c->constant, c->multiplier);
-      fprintf (stdout, "              strength=\"%s\"/>\n", strength_to_string (c->strength));
+          if (range > 0)
+            {
+              squiggly = g_new (char, range + 1);
+              for (int s = 0; s < range; s++)
+                squiggly[s] = '~';
+              squiggly[range] = '\0';
+            }
+
+          fprintf (stderr, "%s: error: %s\n", argv[0], error->message);
+          fprintf (stderr, "%s\n", argv[1]);
+          fprintf (stderr, "%*s^%s\n", offset, " ", squiggly != NULL ? squiggly : "");
+
+          g_free (squiggly);
+
+          die ();
+        }
+
+      int n_constraints = 0;
+      VflConstraint *constraints = vfl_parser_get_constraints (parser, &n_constraints);
+
+      for (int j = 0; j < n_constraints; j++)
+        {
+          VflConstraint *c = &constraints[j];
+
+          fprintf (stdout, "  <constraint target-object=\"%s\" target-attr=\"%s\"\n", c->view1, c->attr1);
+          fprintf (stdout, "              relation=\"%s\"\n", relation_to_string (c->relation));
+
+          if (c->view2 != NULL)
+            fprintf (stdout, "              source-object=\"%s\" source-attr=\"%s\"\n", c->view2, c->attr2);
+
+          fprintf (stdout, "              constant=\"%g\" multiplier=\"%g\"\n", c->constant, c->multiplier);
+          fprintf (stdout, "              strength=\"%s\"/>\n", strength_to_string (c->strength));
+        }
+
+      g_free (constraints);
     }
+
   fprintf (stdout, "</constraints>\n");
 
-  g_free (constraints);
   vfl_parser_free (parser);
 }
