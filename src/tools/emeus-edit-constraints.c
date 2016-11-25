eac51526b6e2957d64a512daf062ed9b6e06d621
Emmanuele Bassi <ebassi@gnome.org>
2016-11-25 18:26:20 +0000

    tools: Add basic UI template for the editor
    Stored inside a GResource.

--- src/tools/emeus-edit-constraints.c
+++ src/tools/emeus-edit-constraints.c
@@ -26,25 +26,63 @@
 #include <gtk/gtk.h>
 #include <emeus.h>
 
+#include "emeus-vfl-parser-private.h"
+
 G_DECLARE_FINAL_TYPE (EditorApplicationWindow, editor_application_window, EDITOR, APPLICATION_WINDOW, GtkApplicationWindow)
 
 struct _EditorApplicationWindow
 {
   GtkApplicationWindow parent_instance;
+
+  VflParser *vfl_parser;
+
+  GtkWidget *lists_switcher;
+  GtkWidget *lists_stack;
+  GtkWidget *views_listbox;
+  GtkWidget *metrics_listbox;
+  GtkWidget *vfl_text_area;
+  GtkWidget *layout_box;
 };
 
 G_DEFINE_TYPE (EditorApplicationWindow, editor_application_window, GTK_TYPE_APPLICATION_WINDOW)
 
 static void
+editor_application_window_finalize (GObject *gobject)
+{
+  EditorApplicationWindow *self = (EditorApplicationWindow *) gobject;
+
+  vfl_parser_free (self->vfl_parser);
+
+  G_OBJECT_CLASS (editor_application_window_parent_class)->finalize (gobject);
+}
+
+static void
 editor_application_window_class_init (EditorApplicationWindowClass *klass)
 {
+  G_OBJECT_CLASS (klass)->finalize = editor_application_window_finalize;
 
+  g_type_ensure (EMEUS_TYPE_CONSTRAINT_LAYOUT);
+
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+  gtk_widget_class_set_template_from_resource (widget_class, "/com/endlessm/EmeusEditor/editor-window.ui");
+  gtk_widget_class_bind_template_child (widget_class, EditorApplicationWindow, lists_switcher);
+  gtk_widget_class_bind_template_child (widget_class, EditorApplicationWindow, lists_stack);
+  gtk_widget_class_bind_template_child (widget_class, EditorApplicationWindow, views_listbox);
+  gtk_widget_class_bind_template_child (widget_class, EditorApplicationWindow, metrics_listbox);
+  gtk_widget_class_bind_template_child (widget_class, EditorApplicationWindow, vfl_text_area);
+  gtk_widget_class_bind_template_child (widget_class, EditorApplicationWindow, layout_box);
 }
 
 static void
 editor_application_window_init (EditorApplicationWindow *self)
 {
-  gtk_window_set_title (GTK_WINDOW (self), "Constraint Editor");
+  gtk_widget_init_template (GTK_WIDGET (self));
+
+  GtkWindow *window = GTK_WINDOW (self);
+  gtk_window_set_title (window, "Constraint Editor");
+  gtk_window_set_default_size (window, 600, 400);
+
+  self->vfl_parser = vfl_parser_new (-1, -1, NULL, NULL);
 }
 
 G_DECLARE_FINAL_TYPE (EditorApplication, editor_application, EDITOR, APPLICATION, GtkApplication)
